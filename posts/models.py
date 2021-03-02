cc68684d8fa73d042c1bcf40d517e09495713cc6
Gauravjot Garaya <gauravjotg@gmail.com>
2021-03-02 00:10:54 -0800

    using timestamp now instead

--- posts/models.py
+++ posts/models.py
@@ -9,8 +9,8 @@ class Posts(models.Model):
     post_text = models.TextField()
     post_image = models.URLField(default="")
     likes = models.JSONField(default=dict)
-    created = models.DateTimeField(default=current_time)
-    updated = models.DateTimeField(default=current_time)
+    created = models.FloatField(default=current_time)
+    updated = models.FloatField(default=current_time)
 
     def __str__(self):
         return "post id:"+str(self.pk) + ", by person: " + str(self.person)
