947b97d3da026988b733735886ed804d3de62ed4
Gauravjot Garaya <gauravjotg@gmail.com>
2021-03-01 23:54:44 -0800

    using POSIX time instead

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from django.utils import timezone
+import datetime
 
 
 # Create your models here.
 class Posts(models.Model):
+    current_time = datetime.datetime.now().timestamp()
     person_id = models.IntegerField()
     post_text = models.TextField()
     post_image = models.URLField(default="")
     likes = models.JSONField(default=dict)
-    created = models.DateTimeField(default=timezone.now)
-    updated = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=current_time)
+    updated = models.DateTimeField(default=current_time)
 
     def __str__(self):
         return "post id:"+str(self.pk) + ", by person: " + str(self.person)
