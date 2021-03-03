f408c7dee6f7ef0153059300022e3c1409f1b49a
Gauravjot Garaya <gauravjotg@gmail.com>
2021-03-02 20:03:23 -0800

    removing unused fucntion

--- account/models.py
+++ account/models.py
@@ -5,9 +5,6 @@ import datetime
 # Create your models here.
 class Person(models.Model):
 
-    def __default_person_friends():
-        return dict(persons=[])
-
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254, unique=True)
