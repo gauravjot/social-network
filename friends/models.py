c269ecce263c38280d4e4cd65308f1c5a55a4b87
Gauravjot Garaya <gauravjotg@gmail.com>
2021-03-02 20:48:58 -0800

    minor bug fixes

--- friends/models.py
+++ friends/models.py
@@ -8,7 +8,7 @@ class Friend(models.Model):
     since = models.FloatField()
 
     def __str__(self):
-        return "id:"+str(self.pk) + ", " + self.user_a + " w/ " + self.user_b+ ", " + self.since
+        return "id: "+str(self.pk) + " => " + str(self.user_a) + " w/ " + str(self.user_b)
 
 class FriendRequest(models.Model):
     from_user = models.IntegerField()
@@ -16,4 +16,4 @@ class FriendRequest(models.Model):
     since = models.FloatField()
 
     def __str__(self):
-        return "from user: "+str(self.from_user)+" to user: " +str(self.to_user)+" - " +str(self.accepted)
+        return "from user: "+str(self.from_user)+" to user: " +str(self.to_user)
