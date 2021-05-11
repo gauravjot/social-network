577834d01fc1c11870aaf65ea2be09bf56322754
zorig <zorig.ganbold@gmail.com>
2021-05-11 16:40:38 +0800

    fix: Using f string literals and applying PEP8 style for account model

--- account/models.py
+++ account/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-import datetime
+
 
 def upload_path(instance, filename):
     return '/'.join(['avatars', str(instance.slug), filename])
+
+
 def cover_upload_path(instance, filename):
     return '/'.join(['covers', str(instance.slug), filename])
+
 
 # Create your models here.
 class Person(models.Model):
@@ -17,14 +20,17 @@ class Person(models.Model):
     birthday = models.CharField(max_length=10, blank=True)
     tagline = models.CharField(max_length=200, blank=True, default="")
     slug = models.CharField(max_length=100, unique=True)
-    hometown = models.CharField(max_length=100,blank=True,default="")
-    work = models.CharField(max_length=100,blank=True,default="")
-    cover_image = models.ImageField(blank=True,null=True, upload_to=cover_upload_path)
+    hometown = models.CharField(max_length=100, blank=True, default="")
+    work = models.CharField(max_length=100, blank=True, default="")
+    cover_image = models.ImageField(
+        blank=True, null=True, upload_to=cover_upload_path)
     created = models.FloatField()
     updated = models.FloatField()
 
     def __str__(self):
-        return "id:"+str(self.pk) + ", " + self.first_name + " " + self.last_name+ ", " + self.email
+        return f"id:{self.pk}, {self.first_name} \
+                {self.last_name}, {self.email}"
+
 
 class Token(models.Model):
     token = models.CharField(max_length=100)
@@ -32,4 +38,4 @@ class Token(models.Model):
     created = models.DateTimeField()
 
     def __str__(self):
-        return "person_id: "+str(self.account)
+        return f"person_id: {self.account}"
